ba1348a6af098d10f0530f83449ff84da4b5e1bd
Florin Miron <mironflorin123@yahoo.com>
2026-05-16 03:27:36 +0100

    feat(navigation): implement main site header with routing links

---
+++ src/components/MainNavigation.jsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router";
+import logoImg from "/images/gm-solutions-logo.png";
+
+export default function MainNavigation() {
+  return (
+    <header className="w-full fixed top-0 z-11 bg-[linear-gradient(to_bottom,rgba(245,244,240,0.6)_55%,rgba(245,244,240,0))]">
+      <nav id="main-nav" className="wrapper flex justify-between py-10">
+        <NavLink to="/">
+          <img src={logoImg} className="logo" alt="GM Solutions logo" />
+        </NavLink>
+        <ul className="flex items-center gap-4">
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "nav-link-active" : ""}`
+              }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="services"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "nav-link-active" : ""}`
+              }
+            >
+              Services
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="gallery"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "nav-link-active" : ""}`
+              }
+            >
+              Gallery
+            </NavLink>
+          </li>
+          <li>
+            <NavLink
+              to="contact"
+              className={({ isActive }) =>
+                `nav-link ${isActive ? "nav-link-active" : ""}`
+              }
+            >
+              Contact
+            </NavLink>
+          </li>
+          <NavLink to="contact" className="nav-link bg-black text-white">
+            Get a Quote
+          </NavLink>
+        </ul>
+      </nav>
+    </header>
+  );
+}
