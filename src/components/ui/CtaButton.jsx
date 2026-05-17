76e2fe5faba3dd848d215cc00682ababd3f1ee19
Florin Miron <mironflorin123@yahoo.com>
2026-05-17 06:05:56 +0100

    feat(ui): create reusable CTA button component

---
+++ src/components/ui/CtaButton.jsx
@@ -0,0 +1,13 @@
+export default function CtaButton({ children, type }) {
+  let classes =
+    "cursor-pointer px-4 py-3 font-bold rounded-md transition-all duration-400 ease-out";
+
+  if (type === "primary") {
+    classes += " bg-primary text-white hover:bg-primary-transparent";
+  }
+  if (type === "secondary") {
+    classes += " bg-white text-black border border-black hover:bg-gray";
+  }
+
+  return <button className={classes}>{children}</button>;
+}
