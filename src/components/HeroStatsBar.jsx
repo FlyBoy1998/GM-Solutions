6fe8d365f82519b04f38144bef6ba7f4e3ae8006
Florin Miron <mironflorin123@yahoo.com>
2026-05-17 22:05:51 +0100

    feat(homepage): add reusable HeroStatsBar component

---
+++ src/components/HeroStatsBar.jsx
@@ -0,0 +1,18 @@
+import { heroStats } from "../utils/utils";
+
+import StatItem from "./ui/StatItem";
+
+export default function HeroStatsBar() {
+  return (
+    <div className="absolute flex gap-3 bottom-0 py-4">
+      {heroStats.map((stat) => (
+        <StatItem
+          key={stat.label}
+          label={stat.label}
+          value={stat.value}
+          Icon={stat.icon}
+        />
+      ))}
+    </div>
+  );
+}
