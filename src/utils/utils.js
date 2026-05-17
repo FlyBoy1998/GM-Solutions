5a9c830904c5e0775be259e1fa7da75d9479afbb
Florin Miron <mironflorin123@yahoo.com>
2026-05-17 22:16:56 +0100

    feat(utils): add reusable heroStats data for homepage

---
+++ src/utils/utils.js
@@ -0,0 +1,8 @@
+import { Clock, Folder } from "lucide-react";
+
+const heroStats = [
+  { label: "Projects completed", value: "100+", icon: Folder },
+  { label: "Experience", value: "10+ Years", icon: Clock },
+];
+
+export { heroStats };
