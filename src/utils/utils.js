4f42253304de21b262bf53972898040bed63d630
Florin Miron <mironflorin123@yahoo.com>
2026-06-06 23:12:04 +0100

    feat(utils): create reusable formatDate utility

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -5,4 +5,18 @@ const heroStats = [
   { label: "Experience", value: "10+ Years", icon: Clock },
 ];
 
-export { heroStats };
+function formatDate(date) {
+  if (!date) return;
+
+  const d = new Date(date);
+
+  const formattedDate = Intl.DateTimeFormat("en-US", {
+    day: "numeric",
+    month: "short",
+    year: "numeric",
+  }).format(d);
+
+  return formattedDate;
+}
+
+export { heroStats, formatDate };
