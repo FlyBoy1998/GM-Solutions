22ba8bdde5119a10fbeb3e7a8edd772a77dbc73d
Florin Miron <mironflorin123@yahoo.com>
2026-06-09 21:29:23 +0100

    feat(projects): create ProjectsList component

---
+++ src/components/projects/ProjectsList.jsx
@@ -0,0 +1,17 @@
+import useFilteredProjects from "../../hooks/useFilteredProjects";
+
+import ProjectCard from "./ProjectCard";
+
+export default function ProjectsList() {
+  const { filteredProjects } = useFilteredProjects();
+
+  return (
+    <div className="shrink-0 min-h-0">
+      <ul className="flex flex-col gap-3 overflow-y-auto mt-4">
+        {filteredProjects.map((project) => (
+          <ProjectCard key={project.id} project={project} />
+        ))}
+      </ul>
+    </div>
+  );
+}
