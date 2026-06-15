5eec26520f732fc3a137e45a5eafeece10f4c4b4
Florin Miron <mironflorin123@yahoo.com>
2026-06-15 11:03:22 +0100

    style(accessibility): add focus ring to CTA buttons

--- src/components/ui/CtaButton.jsx
+++ src/components/ui/CtaButton.jsx
@@ -1,6 +1,6 @@
 export default function CtaButton({ children, type }) {
   let classes =
-    "cursor-pointer px-4 py-3 font-bold rounded-md transition-all duration-400 ease-out";
+    "cursor-pointer px-4 py-3 font-bold rounded-md focus-ring transition-all duration-400 ease-out";
 
   if (type === "primary") {
     classes += " bg-primary text-white hover:bg-primary-transparent";
