d65613b3ab6903ac89d294c6691e2e83358a009d
Florin Miron <mironflorin123@yahoo.com>
2026-06-29 14:58:29 +0100

    feat(projects): add mobile tab navigation

---
+++ src/components/projects/ProjectsMobileNavigation.jsx
@@ -0,0 +1,30 @@
+import { useContext } from "react";
+
+import { ProjectsTabsContext } from "../../context/ProjectsTabsContext";
+
+import { FolderOpenDot, Map } from "lucide-react";
+
+import ProjectMobileTabButton from "./ProjectsMobileTabButton";
+
+export default function ProjectsMobileNavigation() {
+  const { activeTab, setActiveTab } = useContext(ProjectsTabsContext);
+
+  return (
+    <div className="sticky bottom-0 w-full py-4 z-9999 border-t-2 border-primary rounded-t-lg bg-surface shadow-lg lg:hidden">
+      <div className="flex">
+        <ProjectMobileTabButton
+          isActive={activeTab === "projects"}
+          Icon={FolderOpenDot}
+          label="Projects"
+          onClick={() => setActiveTab("projects")}
+        />
+        <ProjectMobileTabButton
+          isActive={activeTab === "map"}
+          Icon={Map}
+          label="Map"
+          onClick={() => setActiveTab("map")}
+        />
+      </div>
+    </div>
+  );
+}
