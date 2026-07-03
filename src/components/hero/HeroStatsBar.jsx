cf92ee4cc685fef7567072b25e4e4cc58389d205
Florin Miron <mironflorin123@yahoo.com>
2026-07-03 10:21:30 +0100

    style(hero): adjust stats bar visibility breakpoint

--- src/components/hero/HeroStatsBar.jsx
+++ src/components/hero/HeroStatsBar.jsx
@@ -4,7 +4,7 @@ import StatItem from "../ui/StatItem";
 
 export default function HeroStatsBar() {
   return (
-    <div className="hidden absolute md:flex gap-3 bottom-0 py-8">
+    <div className="hidden absolute gap-3 bottom-0 py-8 xl:flex">
       {heroStats.map((stat) => (
         <StatItem
           key={stat.label}
