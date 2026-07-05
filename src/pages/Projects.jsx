06c5990fa80eea27d3f4c9b59bb1f96308ebc647
Florin Miron <mironflorin123@yahoo.com>
2026-07-05 22:13:57 +0100

    feat(projects): toggle projects and map views

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,17 +1,26 @@
+import { useContext } from "react";
+
+import { ProjectsTabsContext } from "../context/ProjectsTabsContext";
+
+import Main from "../layout/Main";
+
 import ProjectFilters from "../components/projects/ProjectFilters";
 import ProjectsList from "../components/projects/ProjectsList";
 import ProjectsMap from "../components/projects/ProjectsMap";
 
 import useFilteredProjects from "../hooks/useFilteredProjects";
-import Main from "../layout/Main";
 
 export default function Projects() {
+  const { activeTab } = useContext(ProjectsTabsContext);
+
   const { filteredProjects } = useFilteredProjects();
 
   return (
     <Main>
-      <section className="grid grid-cols-[40%_1fr] h-full gap-3 rounded-lg">
-        <div className="flex flex-col nav-offset-height col-span-1">
+      <section className="grid grid-cols-[40%_1fr] relative h-full gap-3 rounded-lg max-lg:grid-cols-1 max-lg:gap-0">
+        <div
+          className={`nav-offset-height flex flex-col col-span-1 ${activeTab === "projects" ? "flex" : "hidden"} lg:flex`}
+        >
           <p className="eyebrow">Our work</p>
           <h1 className="heading-lg mb-3">Projects</h1>
           <p className="mb-5 text-gray-dark">
@@ -23,7 +32,9 @@ export default function Projects() {
           </p>
           <ProjectsList />
         </div>
-        <div className="h-full col-span-1">
+        <div
+          className={`nav-offset-height col-span-1 ${activeTab === "map" ? "block" : "hidden"} lg:block`}
+        >
           <ProjectsMap />
         </div>
       </section>
