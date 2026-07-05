3b6dff1f593164ffd04d6ef902de939780afdc7a
Florin Miron <mironflorin123@yahoo.com>
2026-07-05 22:11:52 +0100

    feat(projects): show mobile tabs on projects page

--- src/layout/RootLayout.jsx
+++ src/layout/RootLayout.jsx
@@ -1,12 +1,20 @@
-import { Outlet } from "react-router";
+import { useLocation, Outlet } from "react-router";
 
 import MainNavigation from "../components/navigation/MainNavigation";
+import MobileNavigation from "../components/navigation/MobileNavigation";
+import ProjectsMobileTabs from "../components/projects/ProjectsMobileTabs";
 
 export default function RootLayout() {
+  const location = useLocation();
+
+  const isProjects = location.pathname === "/projects";
+
   return (
     <>
+      <MobileNavigation />
       <MainNavigation />
       <Outlet />
+      {isProjects && <ProjectsMobileTabs />}
     </>
   );
 }
