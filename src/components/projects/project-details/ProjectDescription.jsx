e86b1463d6bc199fac03a3ee8e3261f3d8663193
Florin Miron <mironflorin123@yahoo.com>
2026-07-11 19:28:50 +0100

    style(projects): improve project description responsive layout

--- src/components/projects/project-details/ProjectDescription.jsx
+++ src/components/projects/project-details/ProjectDescription.jsx
@@ -1,6 +1,6 @@
 export default function ProjectDescription({ project }) {
   return (
-    <div className="mb-4">
+    <div>
       <h2 className="heading-sm font-bold mb-4">Project overview</h2>
       <p className="text-sm leading-8">{project.overview}</p>
     </div>
