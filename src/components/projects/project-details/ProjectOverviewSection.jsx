81845db443d93c047cef88f5187e77f16156a4eb
Florin Miron <mironflorin123@yahoo.com>
2026-07-11 19:28:17 +0100

    style(projects): improve project overview responsive layout

--- src/components/projects/project-details/ProjectOverviewSection.jsx
+++ src/components/projects/project-details/ProjectOverviewSection.jsx
@@ -6,12 +6,12 @@ import WorkCompletedList from "./WorkCompletedList";
 export default function ProjectOverviewSection({ project }) {
   return (
     <section className="section">
-      <div className="flex gap-8">
-        <div className="flex-1 flex flex-col gap-6">
+      <div className="grid grid-cols-2 max-sm:grid-cols-1 max-sm:gap-y-6">
+        <div className="col-span-1 flex flex-col gap-6 max-sm:col-span-full">
           <ProjectDescription project={project} />
           <WorkCompletedList project={project} />
         </div>
-        <div className="flex-1 flex flex-col gap-6">
+        <div className="col-span-1 flex flex-col gap-6 max-sm:col-span-full">
           <ProjectDetailsCard project={project} />
           <MaterialsUsed project={project} />
         </div>
