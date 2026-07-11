38a64b23651cd23ed8d406893b97ee45941699da
Florin Miron <mironflorin123@yahoo.com>
2026-07-11 20:11:28 +0100

    feat(footer): create FooterServices component

---
+++ src/components/footer/FooterServices.jsx
@@ -0,0 +1,12 @@
+export default function FooterServices() {
+  return (
+    <div className="flex-1">
+      <h3 className="mb-4 font-bold text-white">Our Services</h3>
+      <ul className="list-none flex flex-col gap-3 text-white">
+        <li>Full Home Renovations</li>
+        <li>Bathroom Renovations</li>
+        <li>Kitchen Renovations</li>
+      </ul>
+    </div>
+  );
+}
