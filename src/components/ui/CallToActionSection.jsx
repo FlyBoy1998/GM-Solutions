cad40fc58c78de49d9b8bb4cca83c1320718cc78
Florin Miron <mironflorin123@yahoo.com>
2026-07-14 19:17:15 +0100

    style(ui): apply section utility to call to action section

--- src/components/ui/CallToActionSection.jsx
+++ src/components/ui/CallToActionSection.jsx
@@ -4,19 +4,21 @@ import { MessageSquareQuote, ArrowRight } from "lucide-react";
 
 export default function CallToActionSection() {
   return (
-    <div className="flex items-center justify-between py-8 px-14 rounded-md bg-light max-lg:flex-col max-lg:gap-6">
-      <p className="flex items-center gap-3">
-        <span className="flex items-center justify-center w-20 h-20 rounded-full bg-primary-transparent">
-          <MessageSquareQuote size={30} className="text-white" aria-hidden />
-        </span>
-        <span className="text-xl font-bold">
-          Ready to start your renovation?
-        </span>
-      </p>
-      <CtaButton variant="primary">
-        Get your free quote{" "}
-        <ArrowRight size={20} className="inline-block" aria-hidden />
-      </CtaButton>
+    <div className="section">
+      <div className="flex items-center justify-between py-8 px-14 rounded-md bg-light max-lg:flex-col max-lg:gap-6">
+        <p className="flex items-center gap-3">
+          <span className="flex items-center justify-center w-20 h-20 rounded-full bg-primary-transparent">
+            <MessageSquareQuote size={30} className="text-white" aria-hidden />
+          </span>
+          <span className="text-xl font-bold">
+            Ready to start your renovation?
+          </span>
+        </p>
+        <CtaButton variant="primary">
+          Get your free quote{" "}
+          <ArrowRight size={20} className="inline-block" aria-hidden />
+        </CtaButton>
+      </div>
     </div>
   );
 }
