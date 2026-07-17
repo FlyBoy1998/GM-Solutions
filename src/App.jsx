b80d4d68d7f34c5dd88715137dd36da8310851a1
Florin Miron <mironflorin123@yahoo.com>
2026-07-17 12:36:41 +0100

    feat(admin): add admin route

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,16 @@ import { createBrowserRouter, RouterProvider } from "react-router";
 
 import MapContextProvider from "./context/MapContext";
 import ProjectsTabsContextProvider from "./context/ProjectsTabsContext";
+import MobileNavigationContextProvider from "./context/MobileNavigationContext";
 
 import RootLayout from "./layout/RootLayout";
+import Admin from "./pages/Admin";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
-import MobileNavigationContextProvider from "./context/MobileNavigationContext";
 
 const router = createBrowserRouter([
   {
@@ -18,6 +19,7 @@ const router = createBrowserRouter([
     path: "/",
     errorElement: <PageNotFound />,
     children: [
+      { element: <Admin />, path: "admin", children: [] },
       { index: true, element: <Home /> },
       { element: <Services />, path: "services" },
       { element: <Projects />, path: "projects" },
