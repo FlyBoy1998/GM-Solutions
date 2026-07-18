6c0e099633d6f31a8b24475a433c09d7f49a996e
Florin Miron <mironflorin123@yahoo.com>
2026-07-18 17:32:20 +0100

    feat(admin): add admin layout to routes

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import MapContextProvider from "./context/MapContext";
 import ProjectsTabsContextProvider from "./context/ProjectsTabsContext";
 import MobileNavigationContextProvider from "./context/MobileNavigationContext";
 
+import ProtectedRoute from "./components/authentication/ProtectedRoute";
+
 import RootLayout from "./layout/RootLayout";
 import Admin from "./pages/Admin";
+import AdminLogin from "./components/admin/AdminLogin";
+import AdminDashboard from "./components/admin/AdminDashboard";
 import Home from "./pages/Home";
 import Services from "./pages/Services";
 import Projects from "./pages/Projects";
 import ProjectDetails from "./pages/ProjectDetails";
 import Contact from "./pages/Contact";
 import PageNotFound from "./pages/PageNotFound";
+import AdminLayout from "./layout/AdminLayout";
 
 const router = createBrowserRouter([
   {
@@ -20,12 +25,35 @@ const router = createBrowserRouter([
     path: "/",
     errorElement: <PageNotFound />,
     children: [
-      { element: <Admin />, path: "admin", children: [] },
       { index: true, element: <Home /> },
       { element: <Services />, path: "services" },
       { element: <Projects />, path: "projects" },
       { element: <ProjectDetails />, path: "projects/:id" },
       { element: <Contact />, path: "contact" },
+    ],
+  },
+  {
+    element: <AdminLayout />,
+    path: "/admin",
+    children: [
+      {
+        index: true,
+        element: <AdminLogin />,
+      },
+      {
+        element: (
+          <ProtectedRoute>
+            <Admin />
+          </ProtectedRoute>
+        ),
+        path: "dashboard",
+        children: [
+          {
+            index: true,
+            element: <AdminDashboard />,
+          },
+        ],
+      },
     ],
   },
 ]);
