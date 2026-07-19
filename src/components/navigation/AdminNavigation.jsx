e1e7b7c4782e4eb2cc6c87e1950b9c2b0a3d4f16
Florin Miron <mironflorin123@yahoo.com>
2026-07-19 11:02:55 +0100

    feat(admin): add admin navigation

---
+++ src/components/navigation/AdminNavigation.jsx
@@ -0,0 +1,81 @@
+import { NavLink } from "react-router";
+
+import {
+  LayoutDashboard,
+  FolderOpen,
+  Toolbox,
+  Image,
+  Settings,
+} from "lucide-react";
+
+import Logout from "../authentication/Logout";
+
+import logo from "../../../public/images/gm-solutions-logo-dark.png";
+
+export default function AdminNavigation() {
+  return (
+    <nav className="grid-span-1 flex flex-col p-10 bg-midnight-blue">
+      <NavLink to="/admin" className="w-40 mb-6 rounded-md focus-ring">
+        <img src={logo} className="logo" alt="Gm Solutions logo" />
+      </NavLink>
+      <ul className="list-none flex flex-col gap-3">
+        <li>
+          <NavLink
+            to="dashboard"
+            className={({ isActive }) =>
+              `admin-nav-link focus-ring ${isActive ? "bg-gray-transparent" : ""}`
+            }
+          >
+            <LayoutDashboard size={16} aria-hidden />
+            <span>Dashboard</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="projects"
+            className={({ isActive }) =>
+              `admin-nav-link focus-ring ${isActive ? "bg-gray-transparent" : ""}`
+            }
+          >
+            <FolderOpen size={16} aria-hidden />
+            <span>Projects</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="services"
+            className={({ isActive }) =>
+              `admin-nav-link focus-ring ${isActive ? "bg-gray-transparent" : ""}`
+            }
+          >
+            <Toolbox size={16} aria-hidden />
+            <span>Services</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="media"
+            className={({ isActive }) =>
+              `admin-nav-link focus-ring ${isActive ? "bg-gray-transparent" : ""}`
+            }
+          >
+            <Image size={16} aria-hidden />
+            <span>Media</span>
+          </NavLink>
+        </li>
+        <li>
+          <NavLink
+            to="settings"
+            className={({ isActive }) =>
+              `admin-nav-link focus-ring ${isActive ? "bg-gray-transparent" : ""}`
+            }
+          >
+            <Settings size={16} aria-hidden />
+            <span>Settings</span>
+          </NavLink>
+        </li>
+      </ul>
+      <Logout />
+    </nav>
+  );
+}
