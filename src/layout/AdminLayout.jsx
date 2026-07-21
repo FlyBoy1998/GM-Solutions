7668abb1b7e09ba1b064234e46e3bf455372bc8c
Florin Miron <mironflorin123@yahoo.com>
2026-07-21 20:48:01 +0100

    style(admin): adjust sidebar width

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -4,7 +4,7 @@ import AdminAside from "../components/admin/AdminAside";
 
 export default function AdminLayout() {
   return (
-    <div className="grid grid-cols-[24rem_1fr] min-h-screen">
+    <div className="grid grid-cols-[22rem_1fr] min-h-screen">
       <AdminAside />
       <Outlet />
     </div>
