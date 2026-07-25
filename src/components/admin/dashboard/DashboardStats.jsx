cbaefaeb35dbc8102e6c8c600b10eabdc8bf47d8
Florin Miron <mironflorin123@yahoo.com>
2026-07-25 11:54:29 +0100

    feat(admin): add dashboard statistics component

---
+++ src/components/admin/dashboard/DashboardStats.jsx
@@ -0,0 +1,34 @@
+import DashboardStatsCard from "./DashboardStatsCard";
+
+import { ClipboardCheck, Folder, Map, Image } from "lucide-react";
+
+export default function DashboardStats() {
+  return (
+    <div className="grid grid-cols-4 gap-3 col-span-full">
+      <DashboardStatsCard
+        title="Projects completed"
+        index={120}
+        icon={<ClipboardCheck className="text-white" aria-hidden />}
+        bgColor="bg-blue-500"
+      />
+      <DashboardStatsCard
+        title="Active projects"
+        index={5}
+        icon={<Folder className="text-white" aria-hidden />}
+        bgColor="bg-orange-500"
+      />
+      <DashboardStatsCard
+        title="Map Views"
+        index={100}
+        icon={<Map className="text-white" aria-hidden />}
+        bgColor="bg-green-500"
+      />
+      <DashboardStatsCard
+        title="Media Items"
+        index={100}
+        icon={<Image className="text-white" aria-hidden />}
+        bgColor="bg-purple-500"
+      />
+    </div>
+  );
+}
