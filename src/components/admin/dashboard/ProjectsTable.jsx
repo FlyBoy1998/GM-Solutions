39b99607f179e2114ea888de0c1136114403d708
Florin Miron <mironflorin123@yahoo.com>
2026-07-25 11:31:07 +0100

    feat(admin): add projects table component

---
+++ src/components/admin/dashboard/ProjectsTable.jsx
@@ -0,0 +1,25 @@
+import ProjectsTableHead from "./ProjectsTableHead";
+import ProjectsTableRow from "./ProjectsTableRow";
+
+import { projects } from "../../../../dummy_data/data";
+
+export default function ProjectsTable() {
+  const sortedProjects = projects.sort((a, b) => {
+    const dateA = new Date(a.completionDate);
+    const dateB = new Date(b.completionDate);
+    return dateB - dateA;
+  });
+
+  return (
+    <div className="col-span-3 row-start-3 row-end-5 rounded-lg shadow-md">
+      <table className="h-full w-full text-sm">
+        <ProjectsTableHead />
+        <tbody className="text-xs">
+          {sortedProjects.slice(0, 6).map((project) => (
+            <ProjectsTableRow key={project.id} project={project} />
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
