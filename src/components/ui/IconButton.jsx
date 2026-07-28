d0e7bd9e9c7e98b226983632c7990a14fa881952
Florin Miron <mironflorin123@yahoo.com>
2026-07-28 10:16:54 +0100

    style(ui): add variant icon colors to IconButton

--- src/components/ui/IconButton.jsx
+++ src/components/ui/IconButton.jsx
@@ -1,11 +1,14 @@
 export default function IconButton({ variant, Icon, ariaLabel }) {
   let bgColor;
+  let iconClasses;
 
   if (variant === "view" || variant === "edit") {
     bgColor = "bg-blue-100";
+    iconClasses = "text-blue-500";
   }
   if (variant === "delete") {
     bgColor = "bg-red-100";
+    iconClasses = "text-red-500";
   }
 
   return (
@@ -13,7 +16,7 @@ export default function IconButton({ variant, Icon, ariaLabel }) {
       className={`cursor-pointer flex justify-center items-center p-1 rounded-md ${bgColor}`}
       aria-label={ariaLabel}
     >
-      {<Icon size={14} aria-hidden />}
+      {<Icon size={14} className={iconClasses} aria-hidden />}
     </button>
   );
 }
