75b1995905b77a938588f4a87b23a107f4c0f501
Florin Miron <mironflorin123@yahoo.com>
2026-07-28 14:57:02 +0100

    refactor(admin): move AdminAside to auth folder

--- src/components/admin/auth/AdminAside.jsx
+++ src/components/admin/auth/AdminAside.jsx
@@ -1,4 +1,4 @@
-import { useUser } from "../../hooks/useUser";
+import { useUser } from "../../../hooks/useUser";
 
 import AdminAuthAside from "./AdminAuthAside";
 import AdminNavigation from "../navigation/AdminNavigation";
