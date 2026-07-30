e136ad3bec941474d17c1083d0dc844d404c1fc1
Florin Miron <mironflorin123@yahoo.com>
2026-07-30 16:06:52 +0100

    style(admin): refine projects table layout

--- src/components/admin/ui/ProjectsTable.jsx
+++ src/components/admin/ui/ProjectsTable.jsx
@@ -3,7 +3,7 @@ import ProjectsTableRow from "./ProjectsTableRow";
 
 export default function ProjectsTable({ projects }) {
   return (
-    <table className="h-full w-full text-sm">
+    <table className="w-full text-sm border-separate border-spacing-y-5.5">
       <ProjectsTableHead />
       <tbody className="text-xs">
         {projects.map((project) => (
