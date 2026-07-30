19d706bebf0fcfbbfb1cb4295c27c4b1f1e077bf
Florin Miron <mironflorin123@yahoo.com>
2026-07-30 16:07:44 +0100

    style(admin): refine dashboard layout

--- src/components/admin/dashboard/AdminDashboard.jsx
+++ src/components/admin/dashboard/AdminDashboard.jsx
@@ -6,12 +6,13 @@ import RecentLeads from "./RecentLeads";
 
 export default function AdminDashboard() {
   return (
-    <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_1fr] gap-4 h-full p-6 max-lg:grid-rows-[auto_auto_auto_1fr_1fr] overflow-y-auto">
+    <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_1fr] gap-4 p-6 max-lg:grid-rows-[auto_auto_auto_1fr_1fr] overflow-y-auto">
       <div className="hidden col-span-full max-lg:block">
         <HamburgerBtn />
       </div>
       <div className="col-span-full">
         <h1 className="heading-md">Dashboard</h1>
+        <p className="text-gray-dark">Welcome to your dashboard!</p>
       </div>
       <DashboardStats />
       <LatestProjectsTable />
