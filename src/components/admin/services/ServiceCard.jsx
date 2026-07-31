d707331b43823814ba1c6b8460d17cb42f8e5e03
Florin Miron <mironflorin123@yahoo.com>
2026-07-31 16:21:12 +0100

    feat(admin): add service management card component

---
+++ src/components/admin/services/ServiceCard.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+
+import { Pen, Eye } from "lucide-react";
+
+import ToggleButton from "./ToggleButton";
+import ServiceCardButton from "./ServiceCardButton";
+
+export default function ServiceCard({
+  imgUrl,
+  imgAlt,
+  Icon,
+  title,
+  description,
+}) {
+  const [isToggled, setIsToggled] = useState(false);
+
+  return (
+    <div className="col-span-1 flex flex-col gap-4 h-70 p-3 rounded-md bg-light">
+      <div className="relative h-[50%] rounded-md overflow-hidden">
+        <Icon
+          className="absolute bottom-2 left-2 p-2 rounded-md text-white bg-primary"
+          size={40}
+          aria-hidden
+        />
+        <img src={imgUrl} className="object-cover h-full w-full" alt={imgAlt} />
+      </div>
+      <div>
+        <h3 className="text-lg font-bold mb-1">{title}</h3>
+        <p className="text-sm">{description}</p>
+      </div>
+      <div className="flex justify-between items-center">
+        <div className="flex items-center gap-2">
+          <ServiceCardButton Icon={Pen}>Edit</ServiceCardButton>
+          <ServiceCardButton Icon={Eye}>View</ServiceCardButton>
+        </div>
+        <div className="flex items-center gap-2">
+          <ToggleButton
+            isToggled={isToggled}
+            onChange={() => setIsToggled((prev) => !prev)}
+          />
+          <p className="text-xs">Visible</p>
+        </div>
+      </div>
+    </div>
+  );
+}
