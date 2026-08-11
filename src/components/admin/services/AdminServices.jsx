bf90472be7c9981840bc11ce89d62b44a0f76b13
Florin Miron <mironflorin123@yahoo.com>
2026-08-11 22:30:34 +0100

    feat(admin): build services management page

---
+++ src/components/admin/services/AdminServices.jsx
@@ -0,0 +1,36 @@
+import CtaButton from "../../../components/ui/CtaButton";
+import PageHeader from "../ui/PageHeader";
+import ServiceCard from "./ServiceCard";
+import TopServices from "./TopServices";
+import ServicePerformanceOverview from "./ServicePerformanceOverview";
+
+import { serviceCards } from "../../../constants/data";
+
+export default function AdminServices() {
+  return (
+    <div className="grid grid-cols-3 grid-rows-[repeat(3,auto)] gap-4 h-full p-6">
+      <div className="col-span-full flex justify-between items-center">
+        <PageHeader
+          heading="Services"
+          description="Manage the renovation services displayed on your website."
+        />
+        <div>
+          <CtaButton variant="primary">Add Service</CtaButton>
+        </div>
+      </div>
+      <div className="col-span-full grid grid-cols-3 gap-3">
+        {serviceCards.map((card) => (
+          <ServiceCard
+            key={card.description}
+            imgUrl={card.icon}
+            Icon={card.icon}
+            title={card.title}
+            description={card.description}
+          />
+        ))}
+      </div>
+      <TopServices />
+      <ServicePerformanceOverview />
+    </div>
+  );
+}
