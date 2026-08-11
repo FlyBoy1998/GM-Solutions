b00311bd0633d01268e63db59d7a6ff581aa39ef
Florin Miron <mironflorin123@yahoo.com>
2026-08-11 22:46:06 +0100

    style(admin): refine responsive sidebar widths

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -4,7 +4,7 @@ import AdminAside from "../components/admin/auth/AdminAside";
 
 export default function AdminLayout() {
   return (
-    <div className="h-screen grid grid-cols-[22rem_1fr]">
+    <div className="h-screen grid grid-cols-[20rem_1fr] max-lg:grid-cols-[18rem_1fr] max-md:grid-cols-[16rem_1fr]">
       <AdminAside />
       <Outlet />
     </div>
