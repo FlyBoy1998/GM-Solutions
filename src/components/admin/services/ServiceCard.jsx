77ffae92401cd7baba2a4370a2facddd6b1e87cd
Florin Miron <mironflorin123@yahoo.com>
2026-08-14 11:43:02 +0100

    style(admin): refine service card layout

--- src/components/admin/services/ServiceCard.jsx
+++ src/components/admin/services/ServiceCard.jsx
@@ -15,8 +15,8 @@ export default function ServiceCard({
   const [isToggled, setIsToggled] = useState(true);
 
   return (
-    <div className="col-span-1 flex flex-col gap-4 h-70 p-3 rounded-md bg-light">
-      <div className="relative h-[50%] rounded-md overflow-hidden">
+    <div className="col-span-1 flex flex-col gap-4 h-full p-3 rounded-md bg-light">
+      <div className="relative flex-1 rounded-md overflow-hidden">
         <Icon
           className="absolute bottom-2 left-2 p-2 rounded-md text-white bg-primary"
           size={40}
@@ -24,21 +24,23 @@ export default function ServiceCard({
         />
         <img src={imgUrl} className="object-cover h-full w-full" alt={imgAlt} />
       </div>
-      <div>
-        <h3 className="text-lg font-bold mb-1">{title}</h3>
-        <p className="text-sm">{description}</p>
-      </div>
-      <div className="flex justify-between items-center">
-        <div className="flex items-center gap-2">
-          <ServiceCardButton Icon={Pen}>Edit</ServiceCardButton>
-          <ServiceCardButton Icon={Eye}>View</ServiceCardButton>
+      <div className="flex flex-col gap-3">
+        <div>
+          <h3 className="text-lg font-bold mb-1">{title}</h3>
+          <p className="text-sm">{description}</p>
         </div>
-        <div className="flex items-center gap-2">
-          <ToggleButton
-            isToggled={isToggled}
-            onChange={() => setIsToggled((prev) => !prev)}
-          />
-          <p className="text-xs">Visible</p>
+        <div className="flex justify-between items-center">
+          <div className="flex items-center gap-2">
+            <ServiceCardButton Icon={Pen}>Edit</ServiceCardButton>
+            <ServiceCardButton Icon={Eye}>View</ServiceCardButton>
+          </div>
+          <div className="flex items-center gap-2">
+            <ToggleButton
+              isToggled={isToggled}
+              onChange={() => setIsToggled((prev) => !prev)}
+            />
+            <p className="text-xs">Visible</p>
+          </div>
         </div>
       </div>
     </div>
