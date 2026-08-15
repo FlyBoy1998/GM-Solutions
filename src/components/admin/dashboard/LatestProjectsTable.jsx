887329b62700b41acd78dfba9038dbbc7000c539
Florin Miron <mironflorin123@yahoo.com>
2026-08-15 22:05:05 +0100

    style(admin): make latest projects responsive

--- src/components/admin/dashboard/LatestProjectsTable.jsx
+++ src/components/admin/dashboard/LatestProjectsTable.jsx
@@ -10,7 +10,7 @@ export default function LatestProjectsTable() {
   });
 
   return (
-    <div className="col-span-3 row-start-3 row-end-5 py-4 px-4 rounded-lg shadow-md bg-white">
+    <div className="col-span-3 row-start-3 row-end-5 py-4 px-4 rounded-lg shadow-md bg-white max-lg:col-span-full">
       <h3 className="mb-2 font-bold">Latest Projects</h3>
       <ProjectsTable projects={sortedProjects.slice(0, 6)} />
     </div>
