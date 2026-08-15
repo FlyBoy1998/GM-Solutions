b29aec0a77491716c10faa369a0e687b8c1a733e
Florin Miron <mironflorin123@yahoo.com>
2026-08-15 22:04:11 +0100

    feat(admin): build media management page

---
+++ src/components/admin/media/AdminMedia.jsx
@@ -0,0 +1,50 @@
+import { useQuery } from "@tanstack/react-query";
+
+import PageHeader from "../ui/PageHeader";
+import CtaButton from "../../ui/CtaButton";
+import MediaFilters from "./MediaFilters";
+import MediaGrid from "./MediaGrid";
+import StorageUsage from "./StorageUsage";
+import QuickTips from "./QuickTips";
+
+import { getMediaFiles } from "../../../api/api";
+
+export default function AdminMedia() {
+  const {
+    data: mediaFiles,
+    isLoadingMedia,
+    errorMedia,
+  } = useQuery({
+    queryKey: ["mediaFiles"],
+    queryFn: getMediaFiles,
+  });
+
+  let mediaGridContent;
+
+  if (isLoadingMedia) {
+    mediaGridContent = <p>Loading media files...</p>;
+  } else if (!isLoadingMedia && errorMedia) {
+    mediaGridContent = <p>Error</p>;
+  } else {
+    mediaGridContent = <MediaGrid mediaFiles={mediaFiles} />;
+  }
+
+  return (
+    <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_1fr] gap-4 min-h-full p-6 overflow-y-auto">
+      <div className="col-span-full flex justify-between items-center">
+        <PageHeader
+          heading="Media"
+          description="Manage and organize all images and files used on your website."
+        />
+        <div>
+          <CtaButton variant="primary">Upload Files</CtaButton>
+        </div>
+      </div>
+
+      <MediaFilters />
+      {mediaGridContent}
+      <StorageUsage mediaFiles={mediaFiles} />
+      <QuickTips />
+    </div>
+  );
+}
