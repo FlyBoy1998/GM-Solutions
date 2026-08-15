8129f7b068e083fee3bdd4d15c288bb5df1412a9
Florin Miron <mironflorin123@yahoo.com>
2026-08-15 22:07:13 +0100

    style(admin): refine dashboard responsive layout

--- src/components/admin/dashboard/AdminDashboard.jsx
+++ src/components/admin/dashboard/AdminDashboard.jsx
@@ -7,7 +7,7 @@ import RecentLeads from "./RecentLeads";
 
 export default function AdminDashboard() {
   return (
-    <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_1fr] gap-4 p-6 max-lg:grid-rows-[auto_auto_auto_1fr_1fr] overflow-y-auto">
+    <div className="grid grid-cols-4 grid-rows-[auto_auto_1fr_1fr] gap-4 p-6 max-lg:grid-rows-[auto_auto_auto_1fr_1fr] overflow-y-auto max-lg:grid-cols-3">
       <div className="hidden col-span-full max-lg:block">
         <HamburgerBtn />
       </div>
