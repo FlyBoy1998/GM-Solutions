a93869f4876c8696af1150c2dabd91f8fbf3a6b3
Florin Miron <mironflorin123@yahoo.com>
2026-08-15 22:02:29 +0100

    feat(admin): add quick tips component

---
+++ src/components/admin/media/QuickTips.jsx
@@ -0,0 +1,7 @@
+export default function QuickTips() {
+  return (
+    <div className="col-start-4 col-end-5 row-start-4 row-end-5 py-3 px-4 rounded-lg shadow-md bg-white">
+      <h3 className="mb-2 font-bold">Quick Tips</h3>
+    </div>
+  );
+}
