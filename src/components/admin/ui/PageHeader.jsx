731add1e9e3fcb49dc8d83effc2f6a3809e25878
Florin Miron <mironflorin123@yahoo.com>
2026-08-15 22:01:29 +0100

    style(admin): refine page header layout

--- src/components/admin/ui/PageHeader.jsx
+++ src/components/admin/ui/PageHeader.jsx
@@ -1,6 +1,6 @@
 export default function PageHeader({ heading, description }) {
   return (
-    <div>
+    <div className="col-span-full row-start-1 row-end-2">
       <h3 className="heading-md">{heading}</h3>
       <p className="text-gray-dark">{description}</p>
     </div>
