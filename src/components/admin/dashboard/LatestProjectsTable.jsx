19305bb4c0dc00a8f86d65289ebd986dea412211
Florin Miron <mironflorin123@yahoo.com>
2026-08-17 06:24:53 +0100

    style(admin): refine latest projects xl responsive layout

--- src/components/admin/dashboard/LatestProjectsTable.jsx
+++ src/components/admin/dashboard/LatestProjectsTable.jsx
@@ -6,11 +6,12 @@ export default function LatestProjectsTable() {
   const sortedProjects = projects.slice().sort((a, b) => {
     const dateA = new Date(a.completionDate);
     const dateB = new Date(b.completionDate);
+
     return dateB - dateA;
   });
 
   return (
-    <div className="col-span-3 row-start-3 row-end-5 py-4 px-4 rounded-lg shadow-md bg-white max-lg:col-span-full">
+    <div className="col-span-3 row-start-3 row-end-5 py-4 px-4 rounded-lg shadow-md bg-white max-xl:col-span-full">
       <h3 className="mb-2 font-bold">Latest Projects</h3>
       <ProjectsTable projects={sortedProjects.slice(0, 6)} />
     </div>
