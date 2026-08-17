8bb1280fc203a6a00b5e871de56df60212cee64d
Florin Miron <mironflorin123@yahoo.com>
2026-08-17 19:40:25 +0100

    refactor(admin): rename dashboard stats prop to value

--- src/components/admin/dashboard/DashboardStats.jsx
+++ src/components/admin/dashboard/DashboardStats.jsx
@@ -7,25 +7,25 @@ export default function DashboardStats() {
     <div className="grid grid-cols-4 row-start-2 row-end-3 grid-rows-auto gap-3 col-span-full max-lg:grid-cols-2">
       <DashboardStatsCard
         title="Projects completed"
-        index={120}
+        value={120}
         icon={<ClipboardCheck className="text-white" aria-hidden />}
         bgColor="bg-blue-500"
       />
       <DashboardStatsCard
         title="Active projects"
-        index={5}
+        value={5}
         icon={<Folder className="text-white" aria-hidden />}
         bgColor="bg-orange-500"
       />
       <DashboardStatsCard
         title="Map Views"
-        index={100}
+        value={100}
         icon={<Map className="text-white" aria-hidden />}
         bgColor="bg-green-500"
       />
       <DashboardStatsCard
         title="Media Items"
-        index={100}
+        value={100}
         icon={<Image className="text-white" aria-hidden />}
         bgColor="bg-purple-500"
       />
