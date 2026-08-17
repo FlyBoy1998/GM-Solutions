a83a3639162ee86e114d55bbd2c63d3b76ca7087
Florin Miron <mironflorin123@yahoo.com>
2026-08-17 06:08:27 +0100

    feat(admin): enhance page header with actions and mobile nav

--- src/components/admin/ui/PageHeader.jsx
+++ src/components/admin/ui/PageHeader.jsx
@@ -1,8 +1,16 @@
-export default function PageHeader({ heading, description }) {
+import HamburgerBtn from "../../navigation/HamburgerButton";
+
+export default function PageHeader({ heading, description, children }) {
   return (
-    <div className="col-span-full row-start-1 row-end-2">
-      <h3 className="heading-md">{heading}</h3>
-      <p className="text-gray-dark">{description}</p>
+    <div className="col-span-full flex justify-between items-center">
+      <div className="flex flex-col gap-1.5">
+        <h3 className="heading-md">{heading}</h3>
+        <p className="text-gray-dark">{description}</p>
+      </div>
+      {children}
+      <div className="hidden max-lg:block">
+        <HamburgerBtn />
+      </div>
     </div>
   );
 }
