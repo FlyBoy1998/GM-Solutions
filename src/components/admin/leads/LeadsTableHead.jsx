3c24106d50517849edb6110cc4cb5b8bf18193af
Florin Miron <mironflorin123@yahoo.com>
2026-08-17 20:13:46 +0100

    feat(admin): add leads table header

---
+++ src/components/admin/leads/LeadsTableHead.jsx
@@ -0,0 +1,15 @@
+export default function LeadsTableHead() {
+  return (
+    <thead className="font-bold text-gray-dark">
+      <tr>
+        <th className="table-th">Lead</th>
+        <th className="table-th">Contact</th>
+        <th className="table-th">Source</th>
+        <th className="table-th">Status</th>
+        <th className="table-th">Enquiry</th>
+        <th className="table-th">Date</th>
+        <th className="table-th">Actions</th>
+      </tr>
+    </thead>
+  );
+}
