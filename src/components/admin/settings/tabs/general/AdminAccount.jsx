715b4bf6407cbbc127662e381867f0ac4da65386
Florin Miron <mironflorin123@yahoo.com>
2026-08-21 21:42:42 +0100

    style(admin): adjust admin account layout

--- src/components/admin/settings/tabs/general/AdminAccount.jsx
+++ src/components/admin/settings/tabs/general/AdminAccount.jsx
@@ -4,14 +4,14 @@ import adminImg from "../../../../../../public/images/admin.png";
 
 export default function AdminAccount() {
   return (
-    <div className="col-start-1 col-end-3 row-start-2 row-end-3 py-3 px-4 rounded-lg shadow-md bg-white">
+    <div className="col-start-1 col-end-3 row-start-2 row-end-3 flex flex-col py-3 px-4 rounded-lg shadow-md bg-white">
       <div className="flex flex-col gap-1 mb-6">
         <h3 className="font-bold">Admin Account</h3>
         <p className="text-xs text-gray-dark">
           Manage your administrator account.
         </p>
       </div>
-      <div className="flex justify-between items-center">
+      <div className="flex-1 flex justify-between items-center">
         <div className="flex items-center gap-2">
           <div className="h-6 w-6">
             <img src={adminImg} className="object-cover h-full w-full" alt="" />
