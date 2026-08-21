80aed750a0b3f11c29f22c8dbb3b27efe46bf4e9
Florin Miron <mironflorin123@yahoo.com>
2026-08-21 22:38:36 +0100

    refactor(admin): pass service card data as object

--- src/components/admin/services/ServiceCard.jsx
+++ src/components/admin/services/ServiceCard.jsx
@@ -5,29 +5,23 @@ import { Pen, Eye } from "lucide-react";
 import ToggleButton from "../ui/ToggleButton";
 import ServiceCardButton from "./ServiceCardButton";
 
-export default function ServiceCard({
-  imgUrl,
-  imgAlt,
-  Icon,
-  title,
-  description,
-}) {
+export default function ServiceCard({ card }) {
   const [isToggled, setIsToggled] = useState(true);
 
   return (
     <div className="col-span-1 flex flex-col gap-4 h-full p-3 rounded-md bg-light">
-      <div className="relative flex-1 rounded-md overflow-hidden">
-        <Icon
+      <div className="relative h-40 rounded-md overflow-hidden">
+        <card.icon
           className="absolute bottom-2 left-2 p-2 rounded-md text-white bg-primary"
           size={40}
           aria-hidden
         />
-        <img src={imgUrl} className="object-cover h-full w-full" alt={imgAlt} />
+        <img src={card.imgUrl} className="object-cover h-full w-full" alt="" />
       </div>
       <div className="flex flex-col gap-3">
         <div>
-          <h3 className="text-lg font-bold mb-1">{title}</h3>
-          <p className="text-sm">{description}</p>
+          <h3 className="text-lg font-bold mb-1">{card.title}</h3>
+          <p className="text-sm">{card.description}</p>
         </div>
         <div className="flex justify-between items-center">
           <div className="flex items-center gap-2">
