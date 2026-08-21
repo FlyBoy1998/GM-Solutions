2389e8f28c04183fe2c40d21995ed8c78d64abb6
Florin Miron <mironflorin123@yahoo.com>
2026-08-21 16:45:18 +0100

    feat(admin): add maintenance mode section

---
+++ src/components/admin/settings/tabs/general/MaintenanceMode.jsx
@@ -0,0 +1,22 @@
+import ToggleButton from "../../../ui/ToggleButton";
+
+export default function MaintenanceMode() {
+  return (
+    <div className="col-start-1 col-end-3 row-start-3 row-end-4 flex flex-col py-3 px-4 rounded-lg shadow-md bg-white">
+      <div className="flex flex-col gap-1 mb-6">
+        <h3 className="font-bold">Maintenance Mode</h3>
+        <p className="text-xs text-gray-dark">
+          Temporarily disable the website for maintenance.
+        </p>
+      </div>
+      <div className="flex-1 flex items-center">
+        <div className="flex items-center gap-2">
+          <ToggleButton />
+          <p className="text-xs text-gray-dark">
+            When enabled, only administrators can access the website.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+}
