e5dbc62284ae2625838e91f70a809d628274412b
Florin Miron <mironflorin123@yahoo.com>
2026-08-25 16:31:30 +0100

    style(admin): adjust media card layout

--- src/components/admin/media/MediaCard.jsx
+++ src/components/admin/media/MediaCard.jsx
@@ -7,14 +7,14 @@ export default function MediaCard({ mediaFile }) {
 
   return (
     <article className="col-span-1 flex flex-col rounded-lg shadow-md overflow-hidden bg-white">
-      <div className="h-38 overflow-hidden">
+      <div className="h-46 overflow-hidden">
         <img
           src={mediaFile.imgUrl}
           className={`${imageClasses} ${mediaFile.isImage ? "scale-125" : ""}`}
           alt=""
         />
       </div>
-      <div className="flex flex-col gap-2 p-2">
+      <div className="flex-1 flex flex-col gap-4 p-2">
         <div className="flex items-center justify-between">
           <p className="text-sm font-bold">{mediaFile.name}</p>
           <button className="cursor-pointer">
@@ -24,7 +24,7 @@ export default function MediaCard({ mediaFile }) {
         <p className="text-xs text-gray-dark">
           {`${formatBytes(mediaFile.metadata?.size)}`} • {mediaFile.format}
         </p>
-        <p className="text-xs text-gray-dark">
+        <p className="mt-auto text-xs text-gray-dark">
           {formatDate(mediaFile.created_at.split("T")[0])}
         </p>
       </div>
