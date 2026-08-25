366c43d1775d8f63bb03d3ea469763133bc04db2
Florin Miron <mironflorin123@yahoo.com>
2026-08-25 12:34:54 +0100

    feat(admin): add social media table header

---
+++ src/components/admin/settings/tabs/contact-social/SocialMediaTableHead.jsx
@@ -0,0 +1,13 @@
+export default function SocialMediaTableHead() {
+  return (
+    <thead className="font-bold text-gray-dark">
+      <tr>
+        <th className="table-th">Platform</th>
+        <th className="table-th">URL</th>
+        <th className="table-th">Visible</th>
+        <th className="table-th">Open in New Tab</th>
+        <th className="table-th">Action</th>
+      </tr>
+    </thead>
+  );
+}
