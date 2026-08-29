240c211e7501c9fe3795e8108d6704a096b107b3
Florin Miron <mironflorin123@yahoo.com>
2026-08-29 21:29:08 +0100

    feat(admin): add carousel image item component

---
+++ src/components/admin/forms/project-form/CarouselImageItem.jsx
@@ -0,0 +1,19 @@
+import DeleteItemButton from "../../ui/DeleteItemButton";
+
+export default function CarouselImageItem({ item, onClick }) {
+  return (
+    <div className="flex items-center justify-between p-1 rounded-md border border-gray-dark">
+      <div className="flex items-center gap-2">
+        <div className="h-8 w-12 rounded-md overflow-hidden">
+          <img
+            src={item.imageUrl}
+            className="object-cover h-full w-full"
+            alt=""
+          />
+        </div>
+        <div className="text-xs text-gray-dark">{item.imageName}</div>
+      </div>
+      <DeleteItemButton onClick={onClick} aria-label="Delete Carousel Image" />
+    </div>
+  );
+}
