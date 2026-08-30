7444f03ea1d11d158bc24e49b7a1004b5b9f077f
Florin Miron <mironflorin123@yahoo.com>
2026-08-30 18:39:24 +0100

    feat(admin): add project form validation

--- src/components/admin/forms/project-form/BasicInformation.jsx
+++ src/components/admin/forms/project-form/BasicInformation.jsx
@@ -1,9 +1,16 @@
+import { useFormContext } from "react-hook-form";
+
 import SectionHeader from "../../ui/SectionHeader";
 import FormField from "../../../ui/FormField";
 
 import { projectTypeOptions } from "../../../../constants/data";
 
 export default function BasicInformation() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
     <div className="flex flex-col gap-6 p-4 rounded-lg shadow-md bg-white">
       <SectionHeader
@@ -17,10 +24,17 @@ export default function BasicInformation() {
           inputType="text"
           label="Title"
           id="tile"
-          name="title"
+          {...register("title", {
+            required: "Project title is required.",
+          })}
           placeholder="e.g. Modern Kitchen Remodel"
           required
           additionalStyling="col-span-1"
+          errors={
+            errors.title && (
+              <p className="input-error">{errors.title.message}</p>
+            )
+          }
         />
         <FormField
           type="select"
@@ -28,38 +42,66 @@ export default function BasicInformation() {
           optionsPlaceholder="Select Category"
           label="Category"
           id="category"
-          name="category"
+          {...register("category", {
+            required: "Category is required.",
+          })}
           required
           additionalStyling="col-span-1"
+          errors={
+            errors.category && (
+              <p className="input-error">{errors.category.message}</p>
+            )
+          }
         />
         <FormField
           type="input"
           inputType="text"
           label="Label"
           id="label"
-          name="label"
+          {...register("label", {
+            required: "Label is required.",
+          })}
           placeholder="e.g. Kitchen"
           required
           additionalStyling="col-span-1"
+          errors={
+            errors.label && (
+              <p className="input-error">{errors.label.message}</p>
+            )
+          }
         />
         <FormField
           type="input"
           inputType="text"
           label="Address"
           id="address"
-          name="address"
+          {...register("address", {
+            required: "Address is required.",
+          })}
           placeholder="e.g. Clapham, London"
           required
           additionalStyling="col-span-1"
+          errors={
+            errors.address && (
+              <p className="input-error">{errors.address.message}</p>
+            )
+          }
         />
         <FormField
           type="textarea"
           label="Description"
           id="description"
-          name="description"
+          {...register("description", {
+            required: "Description is required.",
+          })}
           placeholder="A short description of the project..."
           required
           additionalStyling="col-span-full"
+          errors={
+            errors.description && (
+              <p className="input-error">{errors.description.message}</p>
+            )
+          }
         />
       </div>
     </div>
