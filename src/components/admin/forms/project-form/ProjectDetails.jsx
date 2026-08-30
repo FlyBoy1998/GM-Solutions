02995c1739f0fa6f233913c544260656ba053a12
Florin Miron <mironflorin123@yahoo.com>
2026-08-30 18:46:17 +0100

    feat(admin): integrate project details with form validation

--- src/components/admin/forms/project-form/ProjectDetails.jsx
+++ src/components/admin/forms/project-form/ProjectDetails.jsx
@@ -1,7 +1,14 @@
+import { useFormContext } from "react-hook-form";
+
 import SectionHeader from "../../ui/SectionHeader";
 import FormField from "../../../ui/FormField";
 
 export default function ProjectDetails() {
+  const {
+    register,
+    formState: { errors },
+  } = useFormContext();
+
   return (
     <div className="flex flex-col gap-6 p-4 rounded-lg shadow-md bg-white">
       <SectionHeader
@@ -14,48 +21,83 @@ export default function ProjectDetails() {
           inputType="date"
           label="Completion Date"
           id="completion-date"
-          name="completion-date"
+          {...register("completion_date", {
+            required: "Completion date is required.",
+          })}
           required
           additionalStyling="col-span-2"
+          errors={
+            errors.completion_date && (
+              <p className="input-error">{errors.completion_date.message}</p>
+            )
+          }
         />
         <FormField
           type="input"
           inputType="text"
           label="Duration"
           id="duration"
-          name="duration"
+          {...register("duration", {
+            required: "Duration is required.",
+          })}
           placeholder="e.g. 2 weeks"
           required
           additionalStyling="col-span-2"
+          errors={
+            errors.duration && (
+              <p className="input-error">{errors.duration.message}</p>
+            )
+          }
         />
         <FormField
           type="input"
           inputType="text"
           label="Project Size"
           id="project-size"
-          name="project-size"
+          {...register("project_size", {
+            required: "Project size is required.",
+          })}
           placeholder="e.g. 18m2"
           required
           additionalStyling="col-span-2"
+          errors={
+            errors.project_size && (
+              <p className="input-error">{errors.project_size.message}</p>
+            )
+          }
         />
         <FormField
           type="input"
           inputType="text"
           label="Budget Range"
           id="budget-range"
-          name="budget-range"
+          {...register("budget_range", {
+            required: "Budget range is required.",
+          })}
           placeholder="e.g. £18,000 - £35,000"
           required
           additionalStyling="col-span-2"
+          errors={
+            errors.budget_range && (
+              <p className="input-error">{errors.budget_range.message}</p>
+            )
+          }
         />
         <FormField
           type="textarea"
           label="Overview"
           id="overview"
-          name="overview"
+          {...register("overview", {
+            required: "Overview is required.",
+          })}
           placeholder="A detailed overview of the project..."
           required
           additionalStyling="col-span-full"
+          errors={
+            errors.overview && (
+              <p className="input-error">{errors.overview.message}</p>
+            )
+          }
         />
       </div>
     </div>
