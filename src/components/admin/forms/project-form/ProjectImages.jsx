438af1e0162dee77cc383d8a2205984728178f74
Florin Miron <mironflorin123@yahoo.com>
2026-08-30 18:52:20 +0100

    feat(admin): integrate project images with form state

--- src/components/admin/forms/project-form/ProjectImages.jsx
+++ src/components/admin/forms/project-form/ProjectImages.jsx
@@ -1,25 +1,53 @@
+import { useFormContext, Controller } from "react-hook-form";
+
 import SectionHeader from "../../ui/SectionHeader";
 import ImageUpload from "../../ui/ImageUpload";
 
 export default function ProjectImages() {
+  const { control } = useFormContext();
+
   return (
     <div className="flex flex-col gap-6 p-4 rounded-lg shadow-md bg-white">
       <SectionHeader
         title="Project Images"
         description="Add the main images for the project."
       />
-      <ImageUpload
-        id="project-main-img"
+      <Controller
         name="project_main_img"
-        label="Project Thumbnail (Card Image)"
-        required
-      />
-      <ImageUpload
-        id="main-img"
+        control={control}
+        rules={{ required: "Project thumbnail is required." }}
+        render={({ field, fieldState }) => (
+          <ImageUpload
+            label="Project Thumbnail (Card Image)"
+            id="project-main-img"
+            onChange={field.onChange}
+            required
+            errors={
+              fieldState?.error && (
+                <p className="input-error">{fieldState?.error.message}</p>
+              )
+            }
+          />
+        )}
+      ></Controller>
+      <Controller
         name="main_img"
-        label="Project Details Main Image"
-        required
-      />
+        control={control}
+        rules={{ required: "Main image is required." }}
+        render={({ field, fieldState }) => (
+          <ImageUpload
+            label="Project Details Main Image"
+            id="main-img"
+            onChange={field.onChange}
+            required
+            errors={
+              fieldState?.error && (
+                <p className="input-error">{fieldState?.error.message}</p>
+              )
+            }
+          />
+        )}
+      ></Controller>
     </div>
   );
 }
