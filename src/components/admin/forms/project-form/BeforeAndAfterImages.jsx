0452ab30d13b8a56e37140360c6984fd818e719d
Florin Miron <mironflorin123@yahoo.com>
2026-08-30 18:53:24 +0100

    feat(admin): integrate before and after images with form state

--- src/components/admin/forms/project-form/BeforeAndAfterImages.jsx
+++ src/components/admin/forms/project-form/BeforeAndAfterImages.jsx
@@ -1,25 +1,53 @@
+import { Controller, useFormContext } from "react-hook-form";
+
 import SectionHeader from "../../ui/SectionHeader";
 import ImageUpload from "../../ui/ImageUpload";
 
 export default function BeforeAndAfterImages() {
+  const { control } = useFormContext();
+
   return (
     <div className="flex flex-col gap-6 p-4 rounded-lg shadow-md bg-white">
       <SectionHeader
         title="Before & After Images (Popup)"
         description="Add before and after images for the project popup."
       />
-      <ImageUpload
-        label="Before Image"
-        id="popup-before-img"
+      <Controller
         name="popup_before_img"
-        required
-      />
-      <ImageUpload
-        label="After Image"
-        id="popup-after-img"
+        control={control}
+        rules={{ required: "Before image is required." }}
+        render={({ field, fieldState }) => (
+          <ImageUpload
+            label="Before Image"
+            id="popup-before-img"
+            required
+            onChange={field.onChange}
+            errors={
+              fieldState?.error && (
+                <p className="input-error">{fieldState?.error.message}</p>
+              )
+            }
+          />
+        )}
+      ></Controller>
+      <Controller
         name="popup_after_img"
-        required
-      />
+        control={control}
+        rules={{ required: "After image is required." }}
+        render={({ field, fieldState }) => (
+          <ImageUpload
+            label="After Image"
+            id="popup-after-img"
+            required
+            onChange={field.onChange}
+            errors={
+              fieldState?.error && (
+                <p className="input-error">{fieldState?.error.message}</p>
+              )
+            }
+          />
+        )}
+      ></Controller>
     </div>
   );
 }
