eb7597e3a991987ef947f528888d65b1fca5104c
Florin Miron <mironflorin123@yahoo.com>
2026-09-09 11:21:07 +0100

    fix(admin): align project table data with database fields

--- src/components/admin/ui/ProjectsTableRow.jsx
+++ src/components/admin/ui/ProjectsTableRow.jsx
@@ -9,6 +9,10 @@ export default function ProjectsTableRow({ project }) {
   const location = useLocation();
   const navigate = useNavigate();
 
+  const imageUrl = project?.project_images?.find(
+    (image) => image.image_type === "thumbnail",
+  ).storage_path.publicUrl;
+
   let formattedCategory;
   let projectStatusClasses =
     "inline-flex items-center pe-[12px] font-bold rounded-md";
@@ -31,7 +35,7 @@ export default function ProjectsTableRow({ project }) {
     <tr className="border-t border-slate-100 hover:bg-slate-50 transition-colors">
       <td className="table-td whitespace-nowrap">
         <img
-          src={project.imageUrl}
+          src={imageUrl}
           className="object-cover h-12 rounded-lg max-w-none"
           alt={project.alt}
         />
@@ -45,7 +49,7 @@ export default function ProjectsTableRow({ project }) {
           <span>{formatToCapitalize(project.status)}</span>
         </span>
       </td>
-      <td className="table-td">{formatDate(project.completionDate)}</td>
+      <td className="table-td">{formatDate(project.completion_date)}</td>
       <TableActions
         hasDeleteAction={location.pathname.includes("/admin/projects")}
         onEdit={() => navigate(`/admin/projects/${project.id}/edit`)}
