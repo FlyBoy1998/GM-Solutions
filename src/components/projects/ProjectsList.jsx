2b083067977ae0ab6c2ee26d8aeaa01ce4a90d97
Florin Miron <mironflorin123@yahoo.com>
2026-09-12 10:02:18 +0100

    feat(projects): load projects from database

--- src/components/projects/ProjectsList.jsx
+++ src/components/projects/ProjectsList.jsx
@@ -1,15 +1,28 @@
+import useProjects from "../../hooks/useProjects";
 import useFilteredProjects from "../../hooks/useFilteredProjects";
+import toast from "react-hot-toast";
 
 import ProjectCard from "./ProjectCard";
 
 export default function ProjectsList() {
-  const { filteredProjects } = useFilteredProjects();
+  const { data: projects = [], isLoading, error } = useProjects();
+  const { filteredProjects } = useFilteredProjects(projects);
 
   return (
     <ul className="flex flex-col gap-3 overflow-y-auto mt-4 py-3">
-      {filteredProjects.map((project) => (
-        <ProjectCard key={project.id} project={project} />
-      ))}
+      {filteredProjects.map((project) => {
+        const thumbnail = project?.project_images.find(
+          (item) => item.image_type === "thumbnail",
+        ).storage_path.publicUrl;
+
+        return (
+          <ProjectCard
+            key={project.id}
+            project={project}
+            thumbnail={thumbnail}
+          />
+        );
+      })}
     </ul>
   );
 }
