cf2606624f37887182fe1dce2b58e7ae10d52604
Florin Miron <mironflorin123@yahoo.com>
2026-09-12 10:01:02 +0100

    feat(admin): load projects from database

--- src/components/admin/projects/ProjectsManagementTable.jsx
+++ src/components/admin/projects/ProjectsManagementTable.jsx
@@ -3,11 +3,13 @@ import Pagination from "../ui/Pagination";
 
 import usePagination from "../../../hooks/usePagination";
 
-import { projects } from "../../../../dummy_data/data";
+import useProjects from "../../../hooks/useProjects";
 
 export default function ProjectsManagementTable() {
+  const { data: projects = [], isLoading, error } = useProjects();
+
   const { currentPage, totalPages, currentData, setCurrentPage } =
-    usePagination(projects || []);
+    usePagination(projects);
 
   return (
     <div className="flex flex-col col-span-3 row-start-3 row-end-5 min-h-135 px-4 rounded-lg shadow-md bg-white max-xl:col-span-full">
