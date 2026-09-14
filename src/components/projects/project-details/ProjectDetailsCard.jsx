10b4554300244ed62df36eb0aa4ec090f8387d35
Florin Miron <mironflorin123@yahoo.com>
2026-09-14 18:21:34 +0100

    feat(projects): display project details from database

--- src/components/projects/project-details/ProjectDetailsCard.jsx
+++ src/components/projects/project-details/ProjectDetailsCard.jsx
@@ -3,11 +3,20 @@ import { projectDetailIcons } from "../../../constants/data";
 import ProjectDetail from "./ProjectDetail";
 
 export default function ProjectDetailsCard({ project }) {
+  const projectDetails = [
+    { label: "Project Type", value: project?.label },
+    { label: "Location", value: project?.address },
+    { label: "Completion Date", value: project?.completion_date },
+    { label: "Duration", value: project?.duration },
+    { label: "Project Size", value: project?.project_size },
+    { label: "Budget Range", value: project?.budget_range },
+  ];
+
   return (
     <div>
       <h2 className="heading-sm font-bold mb-1">Project details</h2>
       <div>
-        {project.projectDetails.map((detail, index, arr) => {
+        {projectDetails.map((detail, index, arr) => {
           const Icon = projectDetailIcons[detail.label];
           const isLastItem = index + 1 === arr.length;
 
