9d3511f0313a20aa1ec4ce7d501669f0b35a72f1
Florin Miron <mironflorin123@yahoo.com>
2026-09-14 15:34:31 +0100

    feat(admin): add material delete action

--- src/components/admin/forms/project-form/MeterialsUsedItem.jsx
+++ src/components/admin/forms/project-form/MeterialsUsedItem.jsx
@@ -1,12 +1,22 @@
+import { X } from "lucide-react";
+
 import { formatToCapitalize } from "../../../../utils/utils";
 
-export default function MaterialsUsedItem({ material }) {
+export default function MaterialsUsedItem({ material, onDelete }) {
   return (
     <span
-      className="p-1 text-xs rounded-md border border-slate-500 bg-slate-100"
+      className="relative p-1 text-xs rounded-md border border-slate-500 bg-slate-100"
       key={material}
     >
       {formatToCapitalize(material)}
+      <button
+        className="cursor-pointer absolute top-[-25%] p-px rounded-full text-white bg-red-500 hover:bg-red-400 transition-colors"
+        title="Delete material"
+        aria-label="Delete Material"
+        onClick={onDelete}
+      >
+        <X size={12} aria-hidden />
+      </button>
     </span>
   );
 }
