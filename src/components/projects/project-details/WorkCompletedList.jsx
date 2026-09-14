ab025dead2eb3069c9a59aba87d26b3fc11263e4
Florin Miron <mironflorin123@yahoo.com>
2026-09-14 17:25:17 +0100

    feat(projects): render work completed items

--- src/components/projects/project-details/WorkCompletedList.jsx
+++ src/components/projects/project-details/WorkCompletedList.jsx
@@ -1,20 +1,12 @@
-import { CircleCheck } from "lucide-react";
+import WorkCompletedItem from "./WorkCompletedItem";
 
 export default function WorkCompletedList({ project }) {
   return (
     <div>
       <h2 className="heading-sm font-bold mb-4">What we did</h2>
       <ul className="space-y-6">
-        {project.workCompleted.map((item) => (
-          <li className="flex items-start gap-2 text-sm" key={item}>
-            <CircleCheck
-              size={20}
-              strokeWidth={3}
-              className="text-primary"
-              aria-hidden
-            />
-            <span>{item}</span>
-          </li>
+        {project?.work_completed?.map((item) => (
+          <WorkCompletedItem key={item.description} item={item} />
         ))}
       </ul>
     </div>
