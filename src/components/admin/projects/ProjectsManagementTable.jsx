e9ec1dfc3bf354a38bda75543f00d4cc62397bad
Florin Miron <mironflorin123@yahoo.com>
2026-09-16 23:30:17 +0100

    feat(admin): conditionally display project pagination

--- src/components/admin/projects/ProjectsManagementTable.jsx
+++ src/components/admin/projects/ProjectsManagementTable.jsx
@@ -8,17 +8,19 @@ import useProjects from "../../../hooks/useProjects";
 export default function ProjectsManagementTable() {
   const { data: projects = [], isLoading, error } = useProjects();
 
-  const { currentPage, totalPages, currentData, setCurrentPage } =
+  const { currentPage, itemsPerPage, totalPages, currentData, setCurrentPage } =
     usePagination(projects);
 
   return (
     <div className="flex flex-col col-span-3 row-start-3 row-end-5 min-h-135 px-4 rounded-lg shadow-md bg-white max-xl:col-span-full">
       <ProjectsTable projects={currentData} />
-      <Pagination
-        totalPages={totalPages}
-        currentPage={currentPage}
-        onPageChange={setCurrentPage}
-      />
+      {projects.length > itemsPerPage ? (
+        <Pagination
+          totalPages={totalPages}
+          currentPage={currentPage}
+          onPageChange={setCurrentPage}
+        />
+      ) : null}
     </div>
   );
 }
