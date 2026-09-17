762dcee5ba2aae35f1cb8c9f8b3a267268077549
Florin Miron <mironflorin123@yahoo.com>
2026-09-17 11:38:17 +0100

    style(projects): adjust project detail text sizing

--- src/components/projects/project-details/ProjectDetail.jsx
+++ src/components/projects/project-details/ProjectDetail.jsx
@@ -3,13 +3,13 @@ import { formatDate } from "../../../utils/utils";
 export default function ProjectDetail({ detail, isLastItem, Icon }) {
   return (
     <div
-      className={`flex py-3 ${!isLastItem ? "border-b border-b-gray-dark" : ""}}`}
+      className={`flex py-3 text-xs ${!isLastItem ? "border-b border-b-gray-dark" : ""}}`}
     >
-      <p className="flex-1 flex items-center gap-2 text-sm">
+      <p className="flex-1 flex items-center gap-2">
         <Icon className="text-primary" aria-hidden />{" "}
         <span>{detail.label}</span>
       </p>
-      <p className="text-sm">
+      <p>
         {detail.label === "Completion Date"
           ? formatDate(detail.value)
           : detail.value}
