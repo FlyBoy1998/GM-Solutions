9834cff3f954150ab83921e6a9bca29ca11d5c60
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 23:25:35 +0100

    refactor(media): adapt media card to new file structure

--- src/components/admin/media/MediaCard.jsx
+++ src/components/admin/media/MediaCard.jsx
@@ -9,23 +9,24 @@ export default function MediaCard({ mediaFile }) {
     <article className="col-span-1 flex flex-col rounded-lg shadow-md overflow-hidden bg-white">
       <div className="h-46 overflow-hidden">
         <img
-          src={mediaFile.imgUrl}
-          className={`${imageClasses} ${mediaFile.isImage ? "scale-125" : ""}`}
-          alt=""
+          src={mediaFile?.url}
+          className={`${imageClasses} ${mediaFile?.type === "image" ? "scale-125" : ""}`}
+          alt="Media file"
         />
       </div>
       <div className="flex-1 flex flex-col gap-4 p-2">
         <div className="flex items-center justify-between">
-          <p className="text-sm font-bold">{mediaFile.name}</p>
+          <p className="text-sm font-bold">{mediaFile?.name}</p>
           <button className="cursor-pointer">
             <EllipsisVertical size={18} />
           </button>
         </div>
         <p className="text-xs text-gray-dark">
-          {`${formatBytes(mediaFile.metadata?.size)}`} • {mediaFile.format}
+          {`${formatBytes(mediaFile?.metadata.size)}`} •{" "}
+          {mediaFile.metadata.mimetype}
         </p>
         <p className="mt-auto text-xs text-gray-dark">
-          {formatDate(mediaFile.created_at.split("T")[0])}
+          {formatDate(mediaFile?.created_at.split("T")[0])}
         </p>
       </div>
     </article>
