d361edb1811bd4cc2d5ec9ee286aad066f3a360a
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 17:43:40 +0100

    feat(projects): add project modal header component

---
+++ src/components/admin/projects/project-modal/ProjectModalHeader.jsx
@@ -0,0 +1,17 @@
+import { X } from "lucide-react";
+
+export default function ProjectModalHeader() {
+  return (
+    <div className="sticky top-0 flex justify-between items-center px-4 py-6 z-50 shadow-md bg-white">
+      <h2 className="heading-sm">Project Details</h2>
+      <form method="dialog">
+        <button
+          className="cursor-pointer p-2 rounded-md hover:bg-gray transition-colors"
+          aria-label="Close Project Modal"
+        >
+          <X size={16} className="text-black" aria-hidden />
+        </button>
+      </form>
+    </div>
+  );
+}
