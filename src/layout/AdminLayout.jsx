1ec637c21e2989373800c6ac1609358b5ad9a853
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 23:31:54 +0100

    feat(admin): integrate mobile header and navigation

--- src/layout/AdminLayout.jsx
+++ src/layout/AdminLayout.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router";
 
 import AdminAside from "../components/admin/auth/AdminAside";
+import AdminMobileHeader from "../components/admin/navigation/AdminMobileHeader";
+import AdminMobileNavigation from "../components/admin/navigation/AdminMobileNavigation";
 
 export default function AdminLayout() {
   return (
-    <div className="h-screen grid grid-cols-[20rem_1fr] max-lg:grid-cols-[18rem_1fr] max-md:grid-cols-[16rem_1fr]">
+    <div className="h-screen grid grid-cols-[20rem_1fr] max-lg:grid-cols-1 max-lg:grid-rows-[auto_1fr]">
+      <AdminMobileHeader />
+      <AdminMobileNavigation />
       <AdminAside />
       <Outlet />
     </div>
