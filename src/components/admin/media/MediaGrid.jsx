12fd4928b02a9f38b6730e40b9c1cd729adfb0ec
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 23:26:54 +0100

    refactor(media): align media card props

--- src/components/admin/media/MediaGrid.jsx
+++ src/components/admin/media/MediaGrid.jsx
@@ -1,8 +1,7 @@
-import usePagination from "../../../hooks/usePagination";
-import { getMediaFileInfo } from "../../../utils/utils";
 import Pagination from "../ui/Pagination";
-
 import MediaCard from "./MediaCard";
+
+import usePagination from "../../../hooks/usePagination";
 
 export default function MediaGrid({ mediaFiles }) {
   const { currentPage, totalPages, currentData, setCurrentPage } =
@@ -11,16 +10,9 @@ export default function MediaGrid({ mediaFiles }) {
   return (
     <div className="flex flex-col col-span-3 row-span-2 p-3 rounded-lg shadow-lg bg-white">
       <div className="flex-1 grid grid-cols-3 gap-4">
-        {currentData?.map((file) => {
-          const mediaFile = getMediaFileInfo(file);
-
-          return (
-            <MediaCard
-              key={`${file.bucket}-${file.id}`}
-              mediaFile={mediaFile}
-            />
-          );
-        })}
+        {currentData?.map((file) => (
+          <MediaCard key={file.id} mediaFile={file} />
+        ))}
       </div>
       <Pagination
         totalPages={totalPages}
