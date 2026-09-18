a4fb882c2ea917d824d6eafd9d6757e5ae9f60c5
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 17:41:15 +0100

    feat(projects): add project modal overview component

---
+++ src/components/admin/projects/project-modal/ProjectModalOverview.jsx
@@ -0,0 +1,8 @@
+export default function ProjectModalOverview({ overview }) {
+  return (
+    <div>
+      <h2 className="heading-xs">Overview</h2>
+      <p className="py-2 text-xs leading-6">{overview}</p>
+    </div>
+  );
+}
