d393486b6630d9af90dff12a83132d292fa0636d
Florin Miron <mironflorin123@yahoo.com>
2026-09-18 23:28:08 +0100

    feat(utils): add pagination page helper

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -135,3 +135,35 @@ export function formatCurrency(value, currency, format) {
 
   return new Intl.NumberFormat("en-GB", options).format(value);
 }
+
+export function getPaginationPages(totalPages, currentPage) {
+  if (totalPages <= 7) {
+    return Array.from({ length: totalPages }, (_, index) => index + 1);
+  }
+
+  if (currentPage <= 4) {
+    return [1, 2, 3, 4, 5, "...", totalPages];
+  }
+
+  if (currentPage >= totalPages - 3) {
+    return [
+      1,
+      "...",
+      totalPages - 4,
+      totalPages - 3,
+      totalPages - 2,
+      totalPages - 1,
+      totalPages,
+    ];
+  }
+
+  return [
+    1,
+    "...",
+    currentPage - 1,
+    currentPage,
+    currentPage + 1,
+    "...",
+    totalPages,
+  ];
+}
