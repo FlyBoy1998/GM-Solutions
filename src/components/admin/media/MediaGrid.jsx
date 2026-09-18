8d945cca466f72ea44f9dcbb73c51f8714260811
Florin Miron <mironflorin123@yahoo.com>
2026-09-19 00:11:52 +0100

    style(media): refine responsive media grid

--- src/components/admin/media/MediaGrid.jsx
+++ src/components/admin/media/MediaGrid.jsx
@@ -8,7 +8,7 @@ export default function MediaGrid({ mediaFiles }) {
     usePagination(mediaFiles || []);
 
   return (
-    <div className="flex flex-col col-span-3 row-span-2 p-3 rounded-lg shadow-lg bg-white">
+    <div className="flex flex-col col-span-full row-start-3 row-end-4 p-3 rounded-lg shadow-lg bg-white">
       <div className="flex-1 grid grid-cols-3 gap-4">
         {currentData?.map((file) => (
           <MediaCard key={file.id} mediaFile={file} />
