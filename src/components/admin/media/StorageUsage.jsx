c0fffc6bfbd1c2c85c2b4340cc184af0a379e7dc
Florin Miron <mironflorin123@yahoo.com>
2026-09-19 00:11:08 +0100

    style(media): improve responsive storage usage layout

--- src/components/admin/media/StorageUsage.jsx
+++ src/components/admin/media/StorageUsage.jsx
@@ -11,7 +11,7 @@ export default function StorageUsage({ mediaFiles }) {
   }, 0);
 
   return (
-    <div className="col-start-4 col-end-5 row-start-3 row-end-4 flex flex-col gap-4 py-3 px-4 rounded-lg shadow-md bg-white">
+    <div className="col-span-2 row-start-4 row-end-5 flex flex-col gap-4 py-3 px-4 rounded-lg shadow-md bg-white max-sm:col-span-full">
       <SectionHeader
         title="Storage Usage"
         description="Monitor your media storage and keep track of available space."
