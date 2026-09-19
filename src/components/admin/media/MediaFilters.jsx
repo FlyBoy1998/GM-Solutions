bddd60b83e8a63a114e404aff144dd8c8c93388d
Florin Miron <mironflorin123@yahoo.com>
2026-09-19 14:11:03 +0100

    style(media): improve responsive filter layout

--- src/components/admin/media/MediaFilters.jsx
+++ src/components/admin/media/MediaFilters.jsx
@@ -6,21 +6,21 @@ import { mediaTypesOptions } from "../../../constants/data";
 
 export default function MediaFilters({ filesNumber }) {
   return (
-    <div className="col-span-full flex justify-between items-center p-4 rounded-lg shadow-md bg-white">
-      <form action="" className="flex gap-4">
+    <div className="col-span-full flex justify-between items-center p-4 rounded-lg shadow-md bg-white max-md:flex-col max-md:gap-4">
+      <form action="" className="grid grid-cols-4 gap-4 max-md:w-full">
         <FormField
           inputType="text"
           id="searchMedia"
           placeholder="Search media..."
           icon={<Search className="text-gray-dark" size={16} aria-hidden />}
-          additionalStyling="w-60"
+          additionalStyling="col-span-2 max-md:col-span-full"
         />
         <FormField
           type="select"
           optionsPlaceholder="All Types"
           options={mediaTypesOptions}
           icon={<List className="text-gray-dark" size={16} aria-hidden />}
-          additionalStyling="w-60"
+          additionalStyling="col-span-2 max-md:col-span-full"
         />
       </form>
       <p className="text-sm">
