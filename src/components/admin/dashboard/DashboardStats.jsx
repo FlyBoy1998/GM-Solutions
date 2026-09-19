16e4497b48f20a14094521a1e5ddeb5767beb836
Florin Miron <mironflorin123@yahoo.com>
2026-09-19 23:34:08 +0100

    style(dashboard): improve responsive stats layout

--- src/components/admin/dashboard/DashboardStats.jsx
+++ src/components/admin/dashboard/DashboardStats.jsx
@@ -4,7 +4,7 @@ import { dashboardStats } from "../../../constants/data";
 
 export default function DashboardStats() {
   return (
-    <div className="grid grid-cols-4 row-start-2 row-end-3 grid-rows-auto gap-3 col-span-full max-lg:grid-cols-2">
+    <div className="grid grid-cols-4 row-start-2 row-end-3 grid-rows-auto gap-3 col-span-full max-xl:grid-cols-2 max-sm:grid-cols-1">
       {dashboardStats.map((item) => (
         <DashboardStatsCard key={item.id} item={item} />
       ))}
