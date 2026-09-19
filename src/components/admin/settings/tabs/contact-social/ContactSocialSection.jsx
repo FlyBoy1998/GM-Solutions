545b8ca5b4735842ca968baed81f4be0a4c20c70
Florin Miron <mironflorin123@yahoo.com>
2026-09-20 00:27:55 +0100

    feat(settings): add contact social section header

--- src/components/admin/settings/tabs/contact-social/ContactSocialSection.jsx
+++ src/components/admin/settings/tabs/contact-social/ContactSocialSection.jsx
@@ -1,15 +1,14 @@
+import SectionHeader from "../../../ui/SectionHeader";
 import ContactSocialForm from "./ContactSocialForm";
 
 export default function ContactSocialSection() {
   return (
     <div className="col-span-full flex flex-col gap-4 py-3 px-4 rounded-lg shadow-md bg-white">
-      <div className="flex flex-col gap-1">
-        <h3 className="font-heading text-2xl font-bold">Contact & Social</h3>
-        <p className="text-sm text-gray-dark">
-          Update your business contact details. These will be displayed on your
-          website.
-        </p>
-      </div>
+      <SectionHeader
+        title="Contact & Social"
+        description="Update your business contact details. These will be displayed on your
+          website."
+      />
       <ContactSocialForm />
     </div>
   );
