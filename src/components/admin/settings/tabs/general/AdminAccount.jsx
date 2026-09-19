f5b0e0d24afb6e8bd4376002484edc2f3b4a1944
Florin Miron <mironflorin123@yahoo.com>
2026-09-20 00:15:47 +0100

    style(settings): improve admin account responsiveness

--- src/components/admin/settings/tabs/general/AdminAccount.jsx
+++ src/components/admin/settings/tabs/general/AdminAccount.jsx
@@ -5,12 +5,12 @@ import adminImg from "../../../../../../public/images/admin.png";
 
 export default function AdminAccount() {
   return (
-    <div className="col-start-1 col-end-3 row-start-2 row-end-3 flex flex-col py-3 px-4 rounded-lg shadow-md bg-white">
+    <div className="col-start-1 col-end-3 row-start-2 row-end-3 flex flex-col py-3 px-4 rounded-lg shadow-md bg-white max-md:col-span-full max-md:row-start-4 max-md:row-end-5">
       <SectionHeader
         title="Admin Account"
         description="Manage your administrator account."
       />
-      <div className="flex-1 flex justify-between items-center">
+      <div className="flex-1 flex justify-between items-center py-6">
         <div className="flex items-center gap-2">
           <div className="h-6 w-6">
             <img src={adminImg} className="object-cover h-full w-full" alt="" />
