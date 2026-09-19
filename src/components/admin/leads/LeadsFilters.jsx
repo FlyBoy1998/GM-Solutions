8677d2b189df9a98e9d2aba83747d7092963e73d
Florin Miron <mironflorin123@yahoo.com>
2026-09-19 23:48:16 +0100

    style(leads): improve responsive filter layout

--- src/components/admin/leads/LeadsFilters.jsx
+++ src/components/admin/leads/LeadsFilters.jsx
@@ -7,27 +7,27 @@ import { leadsStatuses, leadsSources } from "../../../constants/data";
 export default function LeadsFilters() {
   return (
     <div className="col-span-full flex justify-between items-center p-4 rounded-lg shadow-md bg-white">
-      <form action="" className="flex gap-4">
+      <form action="" className="grid grid-cols-6 gap-4 max-xl:w-full">
         <FormField
           inputType="text"
           id="searchLeads"
           placeholder="Search leads..."
           icon={<Search className="text-gray-dark" size={16} aria-hidden />}
-          additionalStyling="w-60"
+          additionalStyling="col-span-2 max-md:col-span-full"
         />
         <FormField
           type="select"
           optionsPlaceholder="All Statuses"
           options={leadsStatuses}
           icon={<Info className="text-gray-dark" size={16} aria-hidden />}
-          additionalStyling="w-60"
+          additionalStyling="col-span-2 max-md:col-span-3 max-sm:col-span-full"
         />
         <FormField
           type="select"
           optionsPlaceholder="All Sources"
           options={leadsSources}
           icon={<Globe className="text-gray-dark" size={16} aria-hidden />}
-          additionalStyling="w-60"
+          additionalStyling="col-span-2 max-md:col-span-3 max-sm:col-span-full"
         />
       </form>
     </div>
