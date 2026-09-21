e59980d11df064b1ec5ff04811298b7132338b78
Florin Miron <mironflorin123@yahoo.com>
2026-09-21 22:27:54 +0100

    feat(services): add service image form

---
+++ src/components/admin/forms/service-form/ServiceImage.jsx
@@ -0,0 +1,42 @@
+import { useFormContext, Controller } from "react-hook-form";
+
+import ImageUpload from "../../ui/ImageUpload";
+import SectionHeader from "../../ui/SectionHeader";
+
+import useService from "../../../../hooks/useService";
+
+export default function ServiceImage({ serviceId }) {
+  const { control } = useFormContext();
+
+  const { data: service } = useService(serviceId);
+
+  console.log(service);
+
+  return (
+    <div className="flex flex-col gap-6 p-4 rounded-lg shadow-md bg-white">
+      <SectionHeader
+        title="Service Images"
+        description="Add images to showcase this service."
+      />
+      <Controller
+        name="thumbnail_image"
+        control={control}
+        rules={{ required: "Service thumbnail image is required." }}
+        render={({ field, fieldState }) => (
+          <ImageUpload
+            initialImage={service?.thumbnail_image?.url}
+            label="Thumbnail Image"
+            id="service-thumbnail-image"
+            required
+            onChange={field.onChange}
+            errors={
+              fieldState?.error && (
+                <p className="input-error">{fieldState?.error.message}</p>
+              )
+            }
+          />
+        )}
+      ></Controller>
+    </div>
+  );
+}
