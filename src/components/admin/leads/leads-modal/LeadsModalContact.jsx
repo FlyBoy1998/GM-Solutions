dd27c4810db1f085da0320df67460b24d00ba9ac
Florin Miron <mironflorin123@yahoo.com>
2026-09-26 21:05:25 +0100

    feat(leads): add lead contact details section

---
+++ src/components/admin/leads/leads-modal/LeadsModalContact.jsx
@@ -0,0 +1,38 @@
+import { User, Mail, Phone, MessageCircle } from "lucide-react";
+
+import LeadItem from "./LeadItem";
+import SectionHeader from "../../ui/SectionHeader";
+
+export default function LeadsModalContact({ lead }) {
+  return (
+    <div className="flex flex-col gap-2">
+      <SectionHeader title="Contact Information" />
+      <div className="grid grid-cols-2 gap-4">
+        <LeadItem
+          className="col-span-1"
+          Icon={User}
+          label="Full Name"
+          value={`${lead?.first_name} ${lead?.last_name}`}
+        />
+        <LeadItem
+          className="col-span-1"
+          Icon={Mail}
+          label="Email Address"
+          value={lead?.email}
+        />
+        <LeadItem
+          className="col-span-1"
+          Icon={Phone}
+          label="Phone Number"
+          value={lead?.phone}
+        />
+        <LeadItem
+          className="col-span-1"
+          Icon={MessageCircle}
+          label="Preferred Contact Method"
+          value={lead?.contact_method}
+        />
+      </div>
+    </div>
+  );
+}
