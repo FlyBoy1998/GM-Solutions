7b2add0576c0625b799b5d8fcb204681b6c7a698
Florin Miron <mironflorin123@yahoo.com>
2026-09-26 21:14:16 +0100

    feat(leads): build lead details modal

---
+++ src/components/admin/leads/leads-modal/LeadModal.jsx
@@ -0,0 +1,35 @@
+import { createPortal } from "react-dom";
+
+import LeadsModalActions from "./LeadsModalActions";
+import LeadsModalContact from "./LeadsModalContact";
+import LeadsModalEnquiry from "./LeadsModalEnquiry";
+import LeadsModalMessage from "./LeadsModalMessage";
+import LeadsModalTimeline from "./LeadsModalTimeline";
+import LeadsModalAdditionalInfo from "./LeadsModalAdditionalInfo";
+import ModalHeader from "../../ui/ModalHeader";
+
+import { formatDate } from "../../../../utils/utils";
+
+export default function LeadModal({ lead, ref }) {
+  return createPortal(
+    <dialog
+      className="modal fixed top-6 left-1/2 w-[calc(100%-3rem)] m-0 max-w-4xl -translate-x-1/2 overflow-y-auto rounded-lg border-0 shadow-2xl backdrop:bg-modal-backdrop"
+      ref={ref}
+    >
+      <ModalHeader modalTitle="Lead Details" />
+      <div className="grid grid-cols-3 gap-6 px-4 py-2">
+        <div className="col-start-1 col-end-3 flex flex-col gap-6">
+          <LeadsModalContact lead={lead} />
+          <LeadsModalEnquiry lead={lead} />
+          <LeadsModalMessage lead={lead} />
+        </div>
+        <div className="col-start-3 col-end-4 flex flex-col gap-4">
+          <LeadsModalTimeline lead={lead} />
+          <LeadsModalAdditionalInfo lead={lead} />
+        </div>
+      </div>
+      <LeadsModalActions date={formatDate(lead.created_at)} />
+    </dialog>,
+    document.getElementById("modal"),
+  );
+}
